159e975a7b8ad7e6edde2ef4c9b3e7bd5ec62b6d
Keita Yamamoto <yamamoto@altonotes.co.jp>
2024-09-15 22:01:14 +0900

    Add FaceRecognizer

--- VisionBasics/FaceRecognizer.swift
+++ VisionBasics/FaceRecognizer.swift
@@ -1,41 +1,146 @@
-import Foundation
+import UIKit
 import Vision
 
 class FaceRecognizer {
-//    lazy var faceLandmarkRequest = VNDetectFaceLandmarksRequest(completionHandler: self.handleDetectedFaceLandmarks)
+    var outputLayer: CALayer?
 
-//    func performVisionRequest(image: CGImage, orientation: CGImagePropertyOrientation) {
-//        // Create a request handler.
-//        let imageRequestHandler = VNImageRequestHandler(cgImage: image,
-//                                                        orientation: orientation,
-//                                                        options: [:])
-//
-//        // Send the requests to the request handler.
-//        DispatchQueue.global(qos: .userInitiated).async {
-//            do {
-//                try imageRequestHandler.perform([faceLandmarkRequest])
-//            } catch let error as NSError {
-//                print("Failed to perform image request: \(error)")
-//                self.presentAlert("Image Request Failed", error: error)
-//                return
-//            }
-//        }
-//    }
-//
-//    fileprivate func handleDetectedFaceLandmarks(request: VNRequest?, error: Error?) {
-//        if let nsError = error as NSError? {
-//            self.presentAlert("Face Landmark Detection Error", error: nsError)
-//            return
-//        }
-//
-//        // Perform drawing on the main thread.
-//        DispatchQueue.main.async {
-//            guard let drawLayer = self.pathLayer,
-//                let results = request?.results as? [VNFaceObservation] else {
-//                    return
-//            }
-//            self.drawFeatures(onFaces: results, onImageWithBounds: drawLayer.bounds)
-//            drawLayer.setNeedsDisplay()
-//        }
-//    }
+    func start(image: CGImage, orientation: CGImagePropertyOrientation) {
+        // Create a request handler.
+        let imageRequestHandler = VNImageRequestHandler(cgImage: image,
+                                                        orientation: orientation,
+                                                        options: [:])
+
+        // TODO quere削除を試す
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            guard let self else { return }
+
+            let request = VNDetectFaceLandmarksRequest(completionHandler: handleDetectedFaceLandmarks)
+            do {
+                try imageRequestHandler.perform([request])
+            } catch let error as NSError {
+                print("Failed to perform image request: \(error)")
+            }
+        }
+    }
+
+    private func handleDetectedFaceLandmarks(request: VNRequest?, error: Error?) {
+        if let error {
+            print(error)
+            return
+        }
+
+        // Perform drawing on the main thread.
+        DispatchQueue.main.async { [weak self] in
+            guard let self, let results = request?.results as? [VNFaceObservation] else {
+                return
+            }
+            drawFeatures(onFaces: results)
+        }
+    }
+
+    private func drawFeatures(onFaces faces: [VNFaceObservation]) {
+        guard let outputLayer else { return }
+
+        let bounds = outputLayer.bounds
+
+        CATransaction.begin()
+        for faceObservation in faces {
+            let faceBounds = boundingBox(forRegionOfInterest: faceObservation.boundingBox, withinImageBounds: bounds)
+            guard let landmarks = faceObservation.landmarks else {
+                continue
+            }
+
+            // Iterate through landmarks detected on the current face.
+            let landmarkLayer = CAShapeLayer()
+            let landmarkPath = CGMutablePath()
+            let affineTransform = CGAffineTransform(scaleX: faceBounds.size.width, y: faceBounds.size.height)
+
+            // Treat eyebrows and lines as open-ended regions when drawing paths.
+            let openLandmarkRegions: [VNFaceLandmarkRegion2D?] = [
+                landmarks.leftEyebrow,
+                landmarks.rightEyebrow,
+                landmarks.faceContour,
+            ]
+
+            // Draw eyes, lips, and nose as closed regions.
+            let closedLandmarkRegions = [
+                landmarks.leftEye,
+                landmarks.rightEye,
+                landmarks.outerLips,
+                landmarks.nose
+            ].compactMap { $0 } // Filter out missing regions.
+
+            // Draw paths for the open regions.
+            for openLandmarkRegion in openLandmarkRegions where openLandmarkRegion != nil {
+                landmarkPath.addPoints(in: openLandmarkRegion!,
+                                       applying: affineTransform,
+                                       closingWhenComplete: false)
+            }
+
+            // Draw paths for the closed regions.
+            for closedLandmarkRegion in closedLandmarkRegions {
+                landmarkPath.addPoints(in: closedLandmarkRegion,
+                                       applying: affineTransform,
+                                       closingWhenComplete: true)
+            }
+
+            // Format the path's appearance: color, thickness, shadow.
+            landmarkLayer.path = landmarkPath
+            landmarkLayer.lineWidth = 2
+            landmarkLayer.strokeColor = UIColor.green.cgColor
+            landmarkLayer.fillColor = nil
+            landmarkLayer.shadowOpacity = 0.75
+            landmarkLayer.shadowRadius = 4
+
+            // Locate the path in the parent coordinate system.
+            landmarkLayer.anchorPoint = .zero
+            landmarkLayer.frame = faceBounds
+            landmarkLayer.transform = CATransform3DMakeScale(1, -1, 1)
+
+            // Add to pathLayer on top of image.
+            outputLayer.addSublayer(landmarkLayer)
+        }
+        CATransaction.commit()
+        outputLayer.setNeedsDisplay()
+    }
+
+
+    private func boundingBox(forRegionOfInterest: CGRect, withinImageBounds bounds: CGRect) -> CGRect {
+        let imageWidth = bounds.width
+        let imageHeight = bounds.height
+
+        // Begin with input rect.
+        var rect = forRegionOfInterest
+
+        // Reposition origin.
+        rect.origin.x *= imageWidth
+        rect.origin.x += bounds.origin.x
+        rect.origin.y = (1 - rect.origin.y) * imageHeight + bounds.origin.y
+
+        // Rescale normalized coordinates.
+        rect.size.width *= imageWidth
+        rect.size.height *= imageHeight
+
+        return rect
+    }
 }
+
+extension CGMutablePath {
+    // Helper function to add lines to a path.
+    func addPoints(in landmarkRegion: VNFaceLandmarkRegion2D,
+                   applying affineTransform: CGAffineTransform,
+                   closingWhenComplete closePath: Bool) {
+        let pointCount = landmarkRegion.pointCount
+
+        // Draw line if and only if path contains multiple points.
+        guard pointCount > 1 else {
+            return
+        }
+        self.addLines(between: landmarkRegion.normalizedPoints, transform: affineTransform)
+
+        if closePath {
+            self.closeSubpath()
+        }
+    }
+}
+
--- VisionBasics/ViewController.swift
+++ VisionBasics/ViewController.swift
@@ -12,100 +12,94 @@ import Vision
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
-    
+
     // Layer into which to draw bounding box paths.
     var pathLayer: CALayer?
-    
-    // Image parameters for reuse throughout app
-    var imageWidth: CGFloat = 0
-    var imageHeight: CGFloat = 0
-    
-    lazy var faceLandmarkRequest = VNDetectFaceLandmarksRequest(completionHandler: self.handleDetectedFaceLandmarks)
-    
+
+    let faceRecognizer = FaceRecognizer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Tapping the image view brings up the photo picker.
+
         let photoTap = UITapGestureRecognizer(target: self, action: #selector(promptPhoto))
         self.view.addGestureRecognizer(photoTap)
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
+
         if imageView.image == nil {
             promptPhoto()
         }
     }
-    
+
     @objc
     func promptPhoto() {
-        
+
         let prompt = UIAlertController(title: "Choose a Photo",
                                        message: "Please choose a photo.",
                                        preferredStyle: .actionSheet)
-        
+
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
-        
+
         func presentCamera(_ _: UIAlertAction) {
             imagePicker.sourceType = .camera
             self.present(imagePicker, animated: true)
         }
-        
+
         let cameraAction = UIAlertAction(title: "Camera",
                                          style: .default,
                                          handler: presentCamera)
-        
+
         func presentLibrary(_ _: UIAlertAction) {
             imagePicker.sourceType = .photoLibrary
             self.present(imagePicker, animated: true)
         }
-        
+
         let libraryAction = UIAlertAction(title: "Photo Library",
                                           style: .default,
                                           handler: presentLibrary)
-        
+
         func presentAlbums(_ _: UIAlertAction) {
             imagePicker.sourceType = .savedPhotosAlbum
             self.present(imagePicker, animated: true)
         }
-        
+
         let albumsAction = UIAlertAction(title: "Saved Albums",
                                          style: .default,
                                          handler: presentAlbums)
-        
+
         let cancelAction = UIAlertAction(title: "Cancel",
                                          style: .cancel,
                                          handler: nil)
-        
+
         prompt.addAction(cameraAction)
         prompt.addAction(libraryAction)
         prompt.addAction(albumsAction)
         prompt.addAction(cancelAction)
-        
+
         self.present(prompt, animated: true, completion: nil)
     }
-    
-    // MARK: - Helper Methods
-    
+
+    // TODO Check what this function do
     func scaleAndOrient(image: UIImage) -> UIImage {
-        
+
         // Set a default value for limiting image size.
         let maxResolution: CGFloat = 640
-        
+
         guard let cgImage = image.cgImage else {
             print("UIImage has no CGImage backing it!")
             return image
         }
-        
+
         // Compute parameters for transform.
         let width = CGFloat(cgImage.width)
         let height = CGFloat(cgImage.height)
         var transform = CGAffineTransform.identity
-        
+
         var bounds = CGRect(x: 0, y: 0, width: width, height: height)
-        
+
         if width > maxResolution ||
             height > maxResolution {
             let ratio = width / height
@@ -117,7 +111,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 bounds.size.height = maxResolution
             }
         }
-        
+
         let scaleRatio = bounds.size.width / width
         let orientation = image.imageOrientation
         switch orientation {
@@ -152,10 +146,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         default:
             transform = .identity
         }
-        
+
         return UIGraphicsImageRenderer(size: bounds.size).image { rendererContext in
             let context = rendererContext.cgContext
-            
+
             if orientation == .right || orientation == .left {
                 context.scaleBy(x: -scaleRatio, y: scaleRatio)
                 context.translateBy(x: -height, y: 0)
@@ -167,9 +161,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             context.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
         }
     }
-    
+
     func presentAlert(_ title: String, error: NSError) {
-        // Always present alert on main thread.
         DispatchQueue.main.async {
             let alertController = UIAlertController(title: title,
                                                     message: error.localizedDescription,
@@ -182,69 +175,68 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             self.present(alertController, animated: true, completion: nil)
         }
     }
-    
+
     // MARK: - UIImagePickerControllerDelegate
-    
+
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         // Dismiss picker, returning to original root viewController.
         dismiss(animated: true, completion: nil)
     }
-    
+
     internal func imagePickerController(_ picker: UIImagePickerController,
                                         didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
         // Extract chosen image.
         let originalImage: UIImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-        
+
         // Display image on screen.
         show(originalImage)
-        
+
         // Convert from UIImageOrientation to CGImagePropertyOrientation.
         let cgOrientation = CGImagePropertyOrientation(originalImage.imageOrientation)
-        
+
         // Fire off request based on URL of chosen photo.
         guard let cgImage = originalImage.cgImage else {
             return
         }
-        performVisionRequest(image: cgImage,
-                             orientation: cgOrientation)
-        
+        faceRecognizer.start(image: cgImage, orientation: cgOrientation)
+
         // Dismiss the picker to return to original view controller.
         dismiss(animated: true, completion: nil)
     }
-    
+
     func show(_ image: UIImage) {
-        
+
         // Remove previous paths & image
         pathLayer?.removeFromSuperlayer()
         pathLayer = nil
         imageView.image = nil
-        
+
         // Account for image orientation by transforming view.
         let correctedImage = scaleAndOrient(image: image)
-        
+
         // Place photo inside imageView.
         imageView.image = correctedImage
-        
+
         // Transform image to fit screen.
         guard let cgImage = correctedImage.cgImage else {
             print("Trying to show an image not backed by CGImage!")
             return
         }
-        
+
         let fullImageWidth = CGFloat(cgImage.width)
         let fullImageHeight = CGFloat(cgImage.height)
-        
+
         let imageFrame = imageView.frame
         let widthRatio = fullImageWidth / imageFrame.width
         let heightRatio = fullImageHeight / imageFrame.height
-        
+
         // ScaleAspectFit: The image will be scaled down according to the stricter dimension.
         let scaleDownRatio = max(widthRatio, heightRatio)
-        
+
         // Cache image dimensions to reference when drawing CALayer paths.
-        imageWidth = fullImageWidth / scaleDownRatio
-        imageHeight = fullImageHeight / scaleDownRatio
-        
+        let imageWidth = fullImageWidth / scaleDownRatio
+        let imageHeight = fullImageHeight / scaleDownRatio
+
         // Prepare pathLayer to hold Vision results.
         let xLayer = (imageFrame.width - imageWidth) / 2
         let yLayer = imageView.frame.minY + (imageFrame.height - imageHeight) / 2
@@ -254,153 +246,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         drawingLayer.position = CGPoint(x: xLayer, y: yLayer)
         drawingLayer.opacity = 0.5
         pathLayer = drawingLayer
-        self.view.layer.addSublayer(pathLayer!)
-    }
-    
-    // MARK: - Vision
-    
-    func performVisionRequest(image: CGImage, orientation: CGImagePropertyOrientation) {
-        
-        // Fetch desired requests based on switch status.
-        let requests = createVisionRequests()
-        // Create a request handler.
-        let imageRequestHandler = VNImageRequestHandler(cgImage: image,
-                                                        orientation: orientation,
-                                                        options: [:])
-        
-        // Send the requests to the request handler.
-        DispatchQueue.global(qos: .userInitiated).async {
-            do {
-                try imageRequestHandler.perform(requests)
-            } catch let error as NSError {
-                print("Failed to perform image request: \(error)")
-                self.presentAlert("Image Request Failed", error: error)
-                return
-            }
-        }
-    }
-    
-    fileprivate func createVisionRequests() -> [VNRequest] {
-        // Break rectangle & face landmark detection into 2 stages to have more fluid feedback in UI.
-        return [faceLandmarkRequest]
-    }
-    
-    fileprivate func handleDetectedFaceLandmarks(request: VNRequest?, error: Error?) {
-        if let nsError = error as NSError? {
-            self.presentAlert("Face Landmark Detection Error", error: nsError)
-            return
-        }
-        // Perform drawing on the main thread.
-        DispatchQueue.main.async {
-            guard let drawLayer = self.pathLayer,
-                let results = request?.results as? [VNFaceObservation] else {
-                    return
-            }
-            self.drawFeatures(onFaces: results, onImageWithBounds: drawLayer.bounds)
-            drawLayer.setNeedsDisplay()
-        }
-    }
-    
-    // MARK: - Path-Drawing
-    
-    fileprivate func boundingBox(forRegionOfInterest: CGRect, withinImageBounds bounds: CGRect) -> CGRect {
-        
-        let imageWidth = bounds.width
-        let imageHeight = bounds.height
-        
-        // Begin with input rect.
-        var rect = forRegionOfInterest
-        
-        // Reposition origin.
-        rect.origin.x *= imageWidth
-        rect.origin.x += bounds.origin.x
-        rect.origin.y = (1 - rect.origin.y) * imageHeight + bounds.origin.y
-        
-        // Rescale normalized coordinates.
-        rect.size.width *= imageWidth
-        rect.size.height *= imageHeight
-        
-        return rect
-    }
-
-    // Facial landmarks are GREEN.
-    fileprivate func drawFeatures(onFaces faces: [VNFaceObservation], onImageWithBounds bounds: CGRect) {
-        CATransaction.begin()
-        for faceObservation in faces {
-            let faceBounds = boundingBox(forRegionOfInterest: faceObservation.boundingBox, withinImageBounds: bounds)
-            guard let landmarks = faceObservation.landmarks else {
-                continue
-            }
-            
-            // Iterate through landmarks detected on the current face.
-            let landmarkLayer = CAShapeLayer()
-            let landmarkPath = CGMutablePath()
-            let affineTransform = CGAffineTransform(scaleX: faceBounds.size.width, y: faceBounds.size.height)
-            
-            // Treat eyebrows and lines as open-ended regions when drawing paths.
-            let openLandmarkRegions: [VNFaceLandmarkRegion2D?] = [
-                landmarks.leftEyebrow,
-                landmarks.rightEyebrow,
-                landmarks.faceContour,
-            ]
-            
-            // Draw eyes, lips, and nose as closed regions.
-            let closedLandmarkRegions = [
-                landmarks.leftEye,
-                landmarks.rightEye,
-                landmarks.outerLips,
-                landmarks.nose
-                ].compactMap { $0 } // Filter out missing regions.
-            
-            // Draw paths for the open regions.
-            for openLandmarkRegion in openLandmarkRegions where openLandmarkRegion != nil {
-                landmarkPath.addPoints(in: openLandmarkRegion!,
-                                       applying: affineTransform,
-                                       closingWhenComplete: false)
-            }
-            
-            // Draw paths for the closed regions.
-            for closedLandmarkRegion in closedLandmarkRegions {
-                landmarkPath.addPoints(in: closedLandmarkRegion,
-                                       applying: affineTransform,
-                                       closingWhenComplete: true)
-            }
-            
-            // Format the path's appearance: color, thickness, shadow.
-            landmarkLayer.path = landmarkPath
-            landmarkLayer.lineWidth = 2
-            landmarkLayer.strokeColor = UIColor.green.cgColor
-            landmarkLayer.fillColor = nil
-            landmarkLayer.shadowOpacity = 0.75
-            landmarkLayer.shadowRadius = 4
-            
-            // Locate the path in the parent coordinate system.
-            landmarkLayer.anchorPoint = .zero
-            landmarkLayer.frame = faceBounds
-            landmarkLayer.transform = CATransform3DMakeScale(1, -1, 1)
-            
-            // Add to pathLayer on top of image.
-            pathLayer?.addSublayer(landmarkLayer)
-        }
-        CATransaction.commit()
+        faceRecognizer.outputLayer = pathLayer
+        view.layer.addSublayer(pathLayer!)
     }
 }
 
-private extension CGMutablePath {
-    // Helper function to add lines to a path.
-    func addPoints(in landmarkRegion: VNFaceLandmarkRegion2D,
-                   applying affineTransform: CGAffineTransform,
-                   closingWhenComplete closePath: Bool) {
-        let pointCount = landmarkRegion.pointCount
-        
-        // Draw line if and only if path contains multiple points.
-        guard pointCount > 1 else {
-            return
-        }
-        self.addLines(between: landmarkRegion.normalizedPoints, transform: affineTransform)
-        
-        if closePath {
-            self.closeSubpath()
-        }
-    }
-}
